6d8df13c50e96f646d0fe984b200fd1efc9c832b
watharaphong <watcharaphong505@gmail.com>
2026-09-02 16:13:40 +0700

    [add] jwt auth

---
+++ pkg/jwtauth/jwtauth.go
@@ -0,0 +1,71 @@
+package jwtauth
+
+import (
+	"math"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type (
+	AuthFactory interface {
+		SignToken() string
+	}
+
+	Claims struct {
+		Id       string `json:"id"`
+		RoleCode int    `json:"role_code"`
+	}
+
+	AuthMapClaims struct {
+		*Claims
+		jwt.RegisteredClaims
+	}
+
+	authConcrete struct {
+		Secret []byte
+		Claims *AuthMapClaims `json:"claims"`
+	}
+
+	accessToken  struct{ *authConcrete }
+	refreshToken struct{ *authConcrete }
+	apiKey       struct{ *authConcrete }
+)
+
+func (a *authConcrete) SignToken() string {
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, a.Claims)
+	ss, _ := token.SignedString(a.Secret)
+	return ss
+}
+
+func now() time.Time {
+	loc, _ := time.LoadLocation("Asia/Bangkok")
+	return time.Now().In(loc)
+}
+
+func jwtTimeDurationCal(t int64) *jwt.NumericDate {
+	return jwt.NewNumericDate(time.Now().Add(time.Duration(t * int64(math.Pow10(9)))))
+}
+
+func jwtTimeRepeatAdaper(t int64) *jwt.NumericDate {
+	return jwt.NewNumericDate(time.Unix(t, 0))
+}
+
+func NewAccessToken(secret string, expiredAt int64, claims *Claims) AuthFactory {
+	return &accessToken{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				Claims: claims,
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "Iwpza.com",
+					Subject:   "access-token",
+					Audience:  []string{"Iwpza.com"},
+					ExpiresAt: jwtTimeDurationCal(expiredAt),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
+}
